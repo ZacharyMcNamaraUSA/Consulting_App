34fca0b134dc4d8421198f6f39feb76717ccf7e6
Zachary McNamara <zachary.mcnamara.github@gmail.com>
2021-04-14 18:12:14 -0500

    Update Address.java
    Used ZonedDateTime for all date/time needs

--- src/Database/Entities/Address.java
+++ src/Database/Entities/Address.java
@@ -2,24 +2,35 @@ package Database.Entities;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 public class Address {
 
    private String lastUpdatedBy;
-   private Timestamp lastUpdate;
+   private ZonedDateTime lastUpdate;
    private String createdBy;
-   private LocalDateTime createDate;
+   private ZonedDateTime createDate;
    private int countryId;
    private String city;
    private int cityId;
-
-   public LocalDateTime getCreateDate() {
+   
+   /** Getter for ZonedDateTime createDate.
+    * @return the ZonedDateTime of this record's initialization.
+    */
+   public ZonedDateTime getCreateDate() {
       return createDate;
    }
-   public void setCreateDate(LocalDateTime createDate) {
+   
+   /** Setter for ZonedDateTime createDate.
+    * @param createDate Input ZonedDateTime to be new value.
+    */
+   public void setCreateDate(ZonedDateTime createDate) {
       this.createDate = createDate;
    }
-
+   
+   /** Getter for createdBy
+    * @return String value of the username that initialized this record.
+    */
    public String getCreatedBy() {
       return createdBy;
    }
@@ -27,10 +38,10 @@ public class Address {
       this.createdBy = createdBy;
    }
 
-   public Timestamp getLastUpdate() {
+   public ZonedDateTime getLastUpdate() {
       return lastUpdate;
    }
-   public void setLastUpdate(Timestamp lastUpdate) {
+   public void setLastUpdate(ZonedDateTime lastUpdate) {
       this.lastUpdate = lastUpdate;
    }
 
@@ -40,27 +51,45 @@ public class Address {
    public void setLastUpdatedBy(String lastUpdatedBy) {
       this.lastUpdatedBy = lastUpdatedBy;
    }
-
+   
+   /** Getter for int countryId.
+    * @return Returns the countryId as an int.
+    */
    public int getCountryId() {
       return countryId;
    }
-
+   
+   /** Setter for int countryId.
+    * @param countryId Input int to be new countryId value.
+    */
    public void setCountryId(int countryId) {
       this.countryId = countryId;
    }
-
+   
+   /** Getter for String city.
+    * @return Returns String containing the city.
+    */
    public String getCity() {
       return city;
    }
-
+   
+   /** Setter for the String city
+    * @param city String input to be new city value.
+    */
    public void setCity(String city) {
       this.city = city;
    }
-
+   
+   /** Getter for cityId value.
+    * @return Returns the cityId value as an int.
+    */
    public int getCityId() {
       return cityId;
    }
-
+   
+   /** Setter for cityId value.
+    * @param cityId Input int to be new cityId value.
+    */
    public void setCityId(int cityId) {
       this.cityId = cityId;
    }
