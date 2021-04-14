d3fdbdc9f27efa10376e1886c697d2c151553832
Zachary McNamara <zachary.mcnamara.github@gmail.com>
2021-04-14 15:31:04 -0500

    Replaced Object DAO with values
    This updated version of AppointmentDaoImpl.java no longer requires an Appointment as input but the data values instead. This functionality is required because the Appointment_ID is generated by the database, meaning an Appointment cannot exist before it has been added to the database then read back.

--- src/Database/DAO/AppointmentsDaoImpl.java
+++ src/Database/DAO/AppointmentsDaoImpl.java
@@ -3,50 +3,69 @@ package Database.DAO;
 import Database.DBQuery;
 import Database.Entities.Appointment;
 import Utilities.TimeUtilities;
-import View_Controller.MainMenuController;
 
-import javax.swing.text.Utilities;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
+import java.sql.SQLException;
+import java.time.*;
 import java.util.ArrayList;
 
-public class AppointmentsDaoImpl implements AppointmentsDao {
-   private ArrayList<Appointment> allAppointments = null;
-
-   public AppointmentsDaoImpl() {
+public class AppointmentDaoImpl implements AppointmentDao {
+   public ArrayList<Appointment> allAppointments = new ArrayList<>();
+   
+   /**
+    * Default constructor that retrieves all Appointment records.
+    */
+   public AppointmentDaoImpl() {
       readDatabaseAppointments();
    }
-
+   
+   /** Method returns an ArrayList<Appointment> of every currently stored Appointment.
+    * @return Returns the ArrayList<Appointment> of every Appointment.
+    */
    public ArrayList<Appointment> getAllAppointments() {
       return allAppointments;
       
    }
-
-   public boolean addAppointmentToDatabase(Appointment appointment) {
+   
+   /** Method creates a new Appointment record in the database.
+    * @param title Input String to be the Appointment's Title.
+    * @param description Input String to be the Appointment's Description.
+    * @param location Input String to be the Appointment's Location.
+    * @param type Input String to be the Appointment's Type.
+    * @param startZDT Input ZonedDateTime to be the Appointment's Start.
+    * @param endZDT Input ZonedDateTime to be the Appointment's End.
+    * @param createZDT Input ZonedDateTime to be the Appointment's Create_Date.
+    * @param createdByUserName Input String to be the Appointment's Created_By.
+    * @param customerId Input int to be the Appointment's Customer_ID.
+    * @param userId Input int to be the Appointment's User_ID.
+    * @param contactId Input int to be the Appointment's Contact_ID.
+    * @return Returns true if a new Appointment record is created, otherwise returns false.
+    */
+   public static boolean addAppointmentToDatabase(String title, String description, String location, String type,
+                                                  ZonedDateTime startZDT, ZonedDateTime endZDT,
+                                                  ZonedDateTime createZDT, String createdByUserName,
+                                                  int customerId, int userId, int contactId) {
       String insertStatement = "INSERT INTO appointments (Title, Description, Location, Type, " +
-              "Start, End, Create_Date, Created_By, Last_Update, Last_Updated_By, Customer_ID, User_ID, Contact_ID" +
+              "Start, End, Create_Date, Created_By, Last_Update, Last_Updated_By, Customer_ID, User_ID, Contact_ID) " +
               "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
       DBQuery.setPreparedStatement(insertStatement);
       PreparedStatement myPreppedInsert = DBQuery.getPreparedStatement();
       try {
 
-         myPreppedInsert.setString(1, appointment.getTitle());
-         myPreppedInsert.setString(2, appointment.getDescription());
-         myPreppedInsert.setString(3, appointment.getLocation());
-         myPreppedInsert.setString(4, appointment.getType());
-         myPreppedInsert.setString(5, TimeUtilities.convertZDTtoLDTString((appointment.getStart())));
-         myPreppedInsert.setString(6, TimeUtilities.convertZDTtoLDTString(appointment.getEnd()));
-         myPreppedInsert.setString(7, TimeUtilities.convertZDTtoLDTString(appointment.getCreateDate()));
-         myPreppedInsert.setString(8, appointment.getCreatedBy());
-         myPreppedInsert.setString(9, TimeUtilities.convertZDTtoLDTString(ZonedDateTime.now()));
-         myPreppedInsert.setString(10, MainMenuController.activeUsers.getUserName());
-         myPreppedInsert.setString(11, Integer.toString(appointment.getCustomerId()));
-         myPreppedInsert.setString(12, Integer.toString(appointment.getUserId()));
-         myPreppedInsert.setInt(13, appointment.getContactId());
+         myPreppedInsert.setString(1, title);
+         myPreppedInsert.setString(2, description);
+         myPreppedInsert.setString(3, location);
+         myPreppedInsert.setString(4, type);
+         myPreppedInsert.setString(5, TimeUtilities.formatZDTAsDatabaseString(startZDT));
+         myPreppedInsert.setString(6, TimeUtilities.formatZDTAsDatabaseString(endZDT));
+         myPreppedInsert.setString(7, TimeUtilities.formatZDTAsDatabaseString(createZDT));
+         myPreppedInsert.setString(8, createdByUserName);
+         myPreppedInsert.setString(9, TimeUtilities.formatZDTAsDatabaseString(ZonedDateTime.now()));
+         myPreppedInsert.setString(10, createdByUserName);
+         myPreppedInsert.setString(11, java.lang.String.valueOf(customerId));
+         myPreppedInsert.setString(12, java.lang.String.valueOf(userId));
+         myPreppedInsert.setString(13, java.lang.String.valueOf(contactId));
 
          return myPreppedInsert.execute();
       } catch (Exception e) {
@@ -55,30 +74,87 @@ public class AppointmentsDaoImpl implements AppointmentsDao {
 
       return false;
    }
+   
+   /** Method updates an existing Appointment record in the database.
+    * @param updateAppointment Input Appointment object that is already updated to the new values.
+    * @throws SQLException if an error occurs during the database connections.
+    */
+   public static void updateAppointment(Appointment updateAppointment) throws SQLException {
 
-   public void updateAppointment(int oldAppointmentId, Appointment newAppointment) {
-      String updateStatement = "UPDATE appointments SET Title = '" + newAppointment.getTitle() + "', " +
-              "Description = '" + newAppointment.getDescription() + "', " +
-              "Location = '" + newAppointment.getLocation() + "', " +
-              "Type = '" + newAppointment.getType() + "', " +
-              "Start '" + TimeUtilities.convertZDTtoLDTString(newAppointment.getStart()) + "', " +
-              "End '" + TimeUtilities.convertZDTtoLDTString(newAppointment.getEnd()) + "', " +
-              "Last_Update '" + TimeUtilities.convertZDTtoLDTString(ZonedDateTime.now()) + "', " +
-              "Last_Updated_By'" + MainMenuController.activeUsers.getUserName() + "', " +
-              "Customer_ID '" + newAppointment.getCustomerId() + "', " +
-              "User_ID '" + newAppointment.getUserId() + "', " +
-              "Contact_ID '" + newAppointment.getContactId() +
-              " WHERE Appointment_ID = "+ oldAppointmentId;
+      String updateStatement = "UPDATE appointments SET Title = ?, Description = ?, Location = ?, Type = ?, Start = ?, " +
+              "End = ?, Create_Date = ?, Created_By = ?, Last_Update = ?, Last_Updated_By = ?, Customer_ID = ?, " +
+              "User_ID = ?, Contact_ID = ? WHERE Appointment_ID = ?";
+
+      DBQuery.setPreparedStatement(updateStatement);
+      PreparedStatement myPrepUpdate = DBQuery.getPreparedStatement();
+
+      // Title
+      myPrepUpdate.setString(1, updateAppointment.getTitle());
+
+      //Description
+      myPrepUpdate.setString(2, updateAppointment.getDescription());
+
+      // Location
+      myPrepUpdate.setString(3, updateAppointment.getLocation());
+
+      // Type
+      myPrepUpdate.setString(4, updateAppointment.getType());
+
+      // Start
+      String startString = TimeUtilities.formatZDTAsDatabaseString(updateAppointment.getStartZonedDateTime());
+      myPrepUpdate.setString(5, startString);
+
+      // End
+      String endString = TimeUtilities.formatZDTAsDatabaseString(updateAppointment.getEndZonedDateTime());
+      myPrepUpdate.setString(6, endString);
+
+      // Create_Date
+      myPrepUpdate.setString(7,
+              TimeUtilities.formatZDTAsDatabaseString(updateAppointment.getCreateDate()));
+
+      // Created_By
+      myPrepUpdate.setString(8, updateAppointment.getCreatedBy());
+
+      // Last_Update
+      myPrepUpdate.setString(9, TimeUtilities.formatZDTAsDatabaseString(ZonedDateTime.now()));
+
+      // Last_Updated_By
+      myPrepUpdate.setString(10, updateAppointment.getLastUpdatedBy());
+
+      // Customer_ID
+      myPrepUpdate.setString(11, String.valueOf(updateAppointment.getCustomerId()));
+
+      // User_ID
+      myPrepUpdate.setString(12, String.valueOf(updateAppointment.getUserId()));
+
+      // Contact_ID
+      myPrepUpdate.setString(13, String.valueOf(updateAppointment.getContactId()));
+
+      // Appointment_ID
+      myPrepUpdate.setString(14, String.valueOf(updateAppointment.getAppointmentID()));
+
+      try {
+         myPrepUpdate.executeUpdate();
+
+      } catch (Exception e) {
+         e.getStackTrace();
+      }
+
 
    }
-
-
-   public boolean deleteAppointment(Appointment apptToDelete) {
-      String deleteStatement = "DELETE FROM appointments WHERE Appointment_ID=" + apptToDelete.getAppointmentId();
+   
+   
+   /** Method attempts to delete an Appointment record.
+    * @param apptToDelete Input Appointment object with the new values to update the record to.
+    * @return Returns true if an Appointment record is deleted and false if not.
+    */
+   public static boolean deleteAppointment(Appointment apptToDelete) {
+      String deleteStatement = "DELETE FROM appointments WHERE Appointment_ID=" + apptToDelete.getAppointmentID();
       DBQuery.setPreparedStatement(deleteStatement);
       PreparedStatement myPreppedDelete = DBQuery.getPreparedStatement();
+      
       try {
-         if (myPreppedDelete.execute())
+            myPreppedDelete.execute();
             return true;
       } catch (Exception e) {
          e.getStackTrace();
@@ -86,47 +162,70 @@ public class AppointmentsDaoImpl implements AppointmentsDao {
 
       return false;
    }
-
-   void readDatabaseAppointments() {
+   
+   /**
+    * Method reads all Appointment records from the database and stores them in an ArrayList<Appointment> named 'allAppointments'.
+    */
+   private void readDatabaseAppointments() {
       String selectStatement = "SELECT * FROM appointments";
       DBQuery.setPreparedStatement(selectStatement);
       PreparedStatement myPrepStatement = DBQuery.getPreparedStatement();
+
+      int apptId;
+      int custId;
+      int contactId;
+      int userId;
+      String title;
+      String descrip;
+      String loc;
+      String typ;
+      String startString;
+      String endString;
+      String createDateString;
+      String createdBy;
+      String lastUpdateString;
+      String lastUpdatedBy;
 
       try {
          ResultSet rs = myPrepStatement.executeQuery();
          while (rs.next()) {
 
-            int apptId = 0;
-            String title = "";
-            String descrip = "";
-            String loc = "";
-            String typ = "";
-            String startString = "";
-            LocalDateTime startLDT = null;
-            String endString = "";
-            LocalDateTime endLDT = null;
-            LocalDateTime createDate = (LocalDateTime.now());
-            String createdBy = "admin";
-            Instant lastUpdate = Instant.now();
-            String lastUpdatedBy = "admin";
+            apptId = rs.getInt("Appointment_ID");
+            title = rs.getString("Title");
+            descrip = rs.getString("Description");
+            loc = rs.getString("Location");
+            typ = rs.getString("Type");
 
-            allAppointments.add(new Appointment(rs.getInt("Appointment_ID"),
-                    rs.getString("Title"),
-                    rs.getString("Description"),
-                    rs.getString("Location"),
-                    rs.getString("Type"),
-                    (LocalDateTime.parse(rs.getString("Start"),
-                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).atZone(TimeUtilities.zoneIdUTC),
-                    (LocalDateTime.parse(rs.getString("End"),
-                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).atZone(TimeUtilities.zoneIdUTC),
-                    (LocalDateTime.parse(rs.getString("Create_Date"),
-                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).atZone(TimeUtilities.zoneIdUTC),
-                    rs.getString("Created_By"),
-                    rs.getObject("Last_Update", Instant.class),
-                    rs.getString("Last_Updated_By"),
-                    rs.getInt("Customer_ID"),
-                    rs.getInt("User_ID"),
-                    rs.getInt("Contact_ID")));
+            startString = rs.getString("Start");
+            ZonedDateTime startZDT = TimeUtilities.parseStringToZonedDateTime(startString);
+
+            endString = rs.getString("End");
+            ZonedDateTime endZDT = TimeUtilities.parseStringToZonedDateTime(endString);
+
+            createDateString = rs.getString("Create_Date");
+            ZonedDateTime createZDT = TimeUtilities.parseStringToZonedDateTime(createDateString);
+
+            createdBy = rs.getString("Created_By");
+
+            lastUpdateString = rs.getString("Last_Update");
+            LocalDateTime lUpdate = LocalDateTime.parse(lastUpdateString, TimeUtilities.dateTimeFormatter);
+            ZonedDateTime lastUpdateZDT = lUpdate.atZone(ZoneId.of("Z"));
+            lastUpdateZDT = lastUpdateZDT.withZoneSameInstant(ZoneId.systemDefault());
+
+            lastUpdatedBy = rs.getString("Last_Updated_By");
+            custId = rs.getInt("Customer_ID");
+            userId = rs.getInt("User_ID");
+            contactId = rs.getInt("Contact_ID");
+
+//   public Appointment(int appointmentId, String title, String description, String location, String type,
+//                    ZonedDateTime start, ZonedDateTime end, ZonedDateTime createDate, String createdBy,
+//                    Instant lastUpdate, String lastUpdatedBy, int customerId, int userId, int contactId) {
+
+            Appointment nAppointment = new Appointment(apptId, title, descrip, loc, typ, startZDT, endZDT,
+                    createZDT, createdBy, lastUpdateZDT, lastUpdatedBy, custId, userId, contactId);
+
+            allAppointments.add(nAppointment);
+
          }
       } catch (Exception e) {
          e.getStackTrace();
